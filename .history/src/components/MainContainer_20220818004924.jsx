255b8e16224d3cbefa7a2ac68fc2491959eea3c0
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2022-08-18 00:49:50 +0200

    lists

--- .history/src/components/MainContainer_20220818004924.jsx
+++ .history/src/components/MainContainer_20220818004924.jsx
@@ -5,6 +5,7 @@ const MainContainer = () => {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
     <HomeContainer />
+
      <div className="py-2 bg-blue-400 flex-1">
      </div>
 
